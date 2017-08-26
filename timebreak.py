50067f89d079edbab2668029e9afc8559cd49e95
jyotiverma321 <jyotiverma8147@gmail.com>
2017-08-27 01:30:47 +0530

    first project time break

---
+++ timebreak.py
@@ -0,0 +1,10 @@
+import webbrowser
+import time
+
+total_break = 3
+break_count = 0
+print("this program started on"+time.ctime())
+while(break_count < total_break):
+    time.sleep(10)
+    webbrowser.open("https://www.youtube.com/watch?v=I3dezFzsNss")
+    break_count=break_count+1
